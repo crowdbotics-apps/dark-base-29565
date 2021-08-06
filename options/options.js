0f8bb36513d26750dfdedb4c58065def7515f3a1
Crowdbotics <team@crowdbotics.com>
2021-08-06 15:01:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dark_base_29565Identifier",
+  url: "https://dark_base_29565Identifier.botics.co",
+  api: "https://dark_base_29565Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
